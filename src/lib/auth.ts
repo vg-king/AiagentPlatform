b8e8775cb2170feb5afe8184b234848597b87294
vg-king <vg3772285@gmail.com>
2025-10-26 15:41:27 +0530

    03authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth, Schema } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db"; // your drizzle instance
+import * as schema from '@/db/schema'
+export const auth = betterAuth({
+    emailAndPassword: { 
+    enabled: true, 
+  }, 
+    database: drizzleAdapter(db, {
+        provider: "pg", // or "mysql", "sqlite"
+        schema:{
+            ...schema,
+        }
+    }),
+});
